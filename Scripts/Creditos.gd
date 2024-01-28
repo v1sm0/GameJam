3edb2fe2dfc9290c91c4aca2c56d32d0c72f4f66
Nadia <nadiadecar@ug.uchile.cl>
2024-01-28 14:28:27 -0300

    Se agrega una cámara al personaje

--- Scripts/Creditos.gd
+++ Scripts/Creditos.gd
@@ -8,7 +8,9 @@ func _ready():
 	pass
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	texto_creditos.position.y -= 1
+	if(texto_creditos.position.y > -2000):
+		texto_creditos.position.y -= 2
+	
 
 func _on_button_pressed():
 	get_tree().change_scene_to_file("res://Scenes/Menus/MenuPrincipal.tscn")
